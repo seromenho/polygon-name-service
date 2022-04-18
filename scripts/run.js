62a37bbdb5c04315d5085e31abe83d2a79f79d76
Ricardo Seromenho <ricardo.seromenho@gmail.com>
2022-04-18 18:06:24 +0100

    setup resolvers and records

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,26 @@
 const main = async () => {
+  // The first return is the deployer, the second is a random account
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
   const domainContract = await domainContractFactory.deploy();
   await domainContract.deployed();
   console.log("Contract deployed to:", domainContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let txn = await domainContract.register("doom");
+  await txn.wait();
+
+  const domainAddress = await domainContract.getAddress("doom");
+  console.log("Owner of domain doom:", domainAddress);
+
+  txn = await domainContract.connect(owner).setRecord("email", "a@email.com");
+  await txn.wait();
+
+  // Trying to set a record that doesn't belong to me!
+  txn = await domainContract
+    .connect(randomPerson)
+    .setRecord("doom", "Haha my domain now!");
+  await txn.wait();
 };
 
 const runMain = async () => {
