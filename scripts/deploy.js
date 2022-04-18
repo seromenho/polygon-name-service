9dfa772622f2afba75e84ca25e8b2378fb56be35
Ricardo Seromenho <ricardo.seromenho@gmail.com>
2022-04-18 22:55:00 +0100

    deploy to Mumbai test net

---
+++ scripts/deploy.js
@@ -0,0 +1,36 @@
+const main = async () => {
+  const domainContractFactory = await hre.ethers.getContractFactory("Domains");
+  const domainContract = await domainContractFactory.deploy("profile");
+  await domainContract.deployed();
+
+  console.log("Contract deployed to:", domainContract.address);
+
+  // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of seromenhos lol
+  let txn = await domainContract.register("seromenho", {
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await txn.wait();
+  console.log("Minted domain seromenho.profile");
+
+  txn = await domainContract.setRecord("seromenho", "Tada!");
+  await txn.wait();
+  console.log("Set record for seromenho.profile");
+
+  const address = await domainContract.getAddress("seromenho");
+  console.log("Owner of domain seromenho:", address);
+
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
